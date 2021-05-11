a671fa50a98be77e16c70e995b69685c955cbd23
schlorp <70897090+schlorp@users.noreply.github.com>
2021-05-11 11:36:11 +0200

    begin shop

---
+++ BoO/shop/Assets/scripts/Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [HideInInspector] public Inventory inv;
+    public GameObject shopmanager;
+
+    public void Start()
+    {
+        inv = shopmanager.GetComponent<Inventory>();
+
+    }
+
+
+    public void buy()
+    {
+        Debug.Log("bought");
+        inv.Points -= 5;
+        Debug.Log(inv.Points);
+    }
+
+
+
+
+}
